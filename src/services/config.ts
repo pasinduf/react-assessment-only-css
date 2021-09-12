044969eb24521cfc5f6f3e89571982fa192d84c2
pasindu <pasindu.f@aeturnum.com>
2021-09-12 13:31:07 +0530

    api

--- src/services/config.ts
+++ src/services/config.ts
@@ -1,10 +1,7 @@
 const config: BaseConfig = {
     development: {
         apiUrl: 'http://www.omdbapi.com/?apikey=8c94bb5a',
-    },
-    production: {
-        apiUrl: 'http://45.32.110.207:8070/api/',
-    },
+    }
 };
 
 export const baseConfig = (env: string) => {
